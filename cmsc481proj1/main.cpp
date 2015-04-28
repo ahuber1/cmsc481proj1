bff266542cafa6e3126b2f8345b12dd88e67c108
ahuber93 <andrew_huber@MacBook.local>
2015-04-28 16:22:47 -0400

    Initial Commit

---
+++ cmsc481proj1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cmsc481proj1
+//
+//  Created by Andrew Huber on 4/28/2015.
+//  Copyright (c) 2015 Andrew Huber. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
